e38b7c777767c8809c89046f27ad731d2407dc8c
Alex Johnson <johnsonalex1@me.com>
2014-10-24 12:26:38 -0400

    Initial commit

---
+++ Expedition Constellation/Assets/ShipMovement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour {
+
+	public float speed;
+
+	public float thrustFactor;
+
+	private float accel = 1f;
+
+	public float turnSpeed;
+
+	public float rollSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if(Input.GetKey(KeyCode.W)) {
+			accel += 0.001f;
+			rigidbody.AddForce(transform.forward * speed * thrustFactor * accel, ForceMode.Acceleration);
+		} else {
+			accel = 1f;
+		}
+		if(Input.GetKey(KeyCode.S)) {
+			rigidbody.AddForce(-transform.forward * speed * thrustFactor * accel, ForceMode.Acceleration);
+		}
+
+		if(Input.GetKey(KeyCode.D)) {
+			rigidbody.AddRelativeTorque(0,0,-speed/rollSpeed, ForceMode.Acceleration);
+		}
+
+		if(Input.GetKey(KeyCode.A)) {
+			rigidbody.AddRelativeTorque(0,0,speed/rollSpeed, ForceMode.Acceleration);
+		}
+
+		if(Input.GetKey(KeyCode.UpArrow)) {
+			rigidbody.AddRelativeTorque(-speed/turnSpeed,0,0, ForceMode.Acceleration);
+		}
+
+		if(Input.GetKey(KeyCode.DownArrow)) {
+			rigidbody.AddRelativeTorque(speed/turnSpeed,0,0, ForceMode.Acceleration);
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			rigidbody.AddRelativeTorque(0,-speed/turnSpeed,0, ForceMode.Acceleration);
+		}
+		
+		if(Input.GetKey(KeyCode.RightArrow)) {
+			rigidbody.AddRelativeTorque(0,speed/turnSpeed,0, ForceMode.Acceleration);
+		}*/
+
+
+	}
+}
---
+++ Expedition Constellation/Assets/Thruster.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Thruster : MonoBehaviour {
+
+	public KeyCode activateKey;//This key will activate these thrusters, for now, thrusters are toggled, but analog versions will be created later
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(activateKey)) {
+			rigidbody.AddRelativeForce(0.0f,0.0f,1.0f);
+		}
+	}
+}
